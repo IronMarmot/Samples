5b4a13e109c6257bd4fc9a56f95a90a0a4da60ee
IronMarmot <35325388+IronMarmot@users.noreply.github.com>
2020-04-24 18:00:06 +0800

    repository create
    add project MyReflection

---
+++ MyReflection/MyReflection/DB.Oracle/OracleHelper.cs
@@ -0,0 +1,26 @@
+﻿using DB.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.Oracle
+{
+    class OracleHelper:IDBHelper
+    {
+        public OracleHelper()
+        {
+            Console.WriteLine("{0}构造函数", this.GetType().Name);
+        }
+        public void Query()
+        {
+            Console.WriteLine("{0}.Query",this.GetType().Name);
+        }
+
+        public void Insert()
+        {
+            Console.WriteLine("{0}.Insert", this.GetType().Name);
+        }
+    }
+}
---
+++ MyReflection/MyReflection/DB.SqlServer/SqlServerHelper.cs
@@ -0,0 +1,26 @@
+﻿using DB.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.SqlServer
+{
+    class SqlServerHelper:IDBHelper
+    {
+        public SqlServerHelper()
+        {
+            Console.WriteLine("{0}构造函数", this.GetType().Name);
+        }
+        public void Query()
+        {
+            Console.WriteLine("{0}.Query",this.GetType().Name);
+        }
+
+        public void Insert()
+        {
+            Console.WriteLine("{0}.Insert", this.GetType().Name);
+        }
+    }
+}
