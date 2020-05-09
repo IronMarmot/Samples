8f3819ff780e1b98dd8d1eacf1304513a375d6da
IronMarmot <35325388+IronMarmot@users.noreply.github.com>
2020-05-09 18:02:41 +0800

    异常处理

---
+++ CoreSamples/15_ExceptionDemo/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _15_ExceptionDemo.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("/error")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
