860e0e84ec01260435d6a85ee511dda8176800e9
IronMarmot <35325388+IronMarmot@users.noreply.github.com>
2020-05-11 17:03:24 +0800

    Revert "Revert "异常Demo""
    This reverts commit 987b2e2e62a723e5202974fdd19c39f4072f81ec.

--- CoreSamples/15_ExceptionDemo/Controllers/ErrorController.cs
+++ CoreSamples/15_ExceptionDemo/Controllers/ErrorController.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using _15_ExceptionDemo.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace _15_ExceptionDemo.Controllers
 {
@@ -11,7 +16,21 @@ namespace _15_ExceptionDemo.Controllers
         [Route("/error")]
         public IActionResult Index()
         {
-            return View();
+            IExceptionHandlerPathFeature exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            Exception ex=exceptionHandlerPathFeature?.Error;
+
+            var knowException = ex as IKnowException;
+            if (knowException==null)
+            {
+                var logger = HttpContext.RequestServices.GetService<ILogger<MyExceptionFilterAttribute>>();
+                logger.LogError(ex,ex.Message);
+                knowException = KnowException.Unkown;
+            }
+            else
+            {
+                knowException = KnowException.FromKnowException(knowException);
+            }
+            return View(knowException);
         }
     }
 }
---
+++ CoreSamples/15_ExceptionDemo/Exceptions/MyExceptionFilter.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _15_ExceptionDemo.Exceptions
+{
+    public class MyExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            IKnowException knowException = context.Exception as IKnowException;
+            if (knowException == null)
+            {
+                knowException = KnowException.Unkown;
+                context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            }
+            else
+            {
+                knowException = KnowException.FromKnowException(knowException);
+                context.HttpContext.Response.StatusCode = StatusCodes.Status200OK;
+            }
+            context.Result = new JsonResult(knowException) { ContentType = "application/json; charset=utf-8" };
+        }
+    }
+}
