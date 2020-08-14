3f7ea4ebcb1bfbfa2ff9f3645aba01cfdef1c528
IronMarmot <35325388+IronMarmot@users.noreply.github.com>
2020-08-14 17:55:55 +0800

    RabbitMq(1)
    最简单的一对一示例

---
+++ RabbitMQ/Receive/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "123.57.233.60" };
+            using (var connection=factory.CreateConnection())
+            {
+                using (var channel=connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    //consumer.Received += Consumer_Received;
+                    consumer.Received += (sender, e) =>
+                    {
+                        var body=e.Body;
+                        string msg = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine($"[x] receive{msg}");
+                    };
+
+                    channel.BasicConsume(queue: "hello",
+                        autoAck: true,
+                        consumer: consumer);
+
+                    Console.WriteLine("press any key exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RabbitMQ/Send/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "123.57.233.60" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    string msg = "hello world";
+                    byte[] bt = Encoding.UTF8.GetBytes(msg);
+
+                    channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: bt);
+
+                    Console.WriteLine(  $"[x] send {msg}");
+                }
+
+                Console.WriteLine("press any key exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
