cbd38735de486d2f22dd7c87acdea962768ceebb
Jack <35325388+IronMarmot@users.noreply.github.com>
2020-08-19 22:42:32 +0800

    一个生产者多个消费者具体实现

--- RabbitMQ/02NewTask/Program.cs
+++ RabbitMQ/02NewTask/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace _02NewTask
 {
@@ -6,7 +8,38 @@ namespace _02NewTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "123.57.233.60" };
+            using (var connection=factory.CreateConnection())
+            {
+                using (var channel=connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "task_queue",
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;//消息持久化
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "task_queue",
+                        basicProperties: properties,
+                        body: body);
+                    Console.WriteLine($" [x] Send {message}");
+                }
+
+                Console.WriteLine(" Press any key to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "hello world!");
         }
     }
 }
--- RabbitMQ/02Worker/Program.cs
+++ RabbitMQ/02Worker/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
 
 namespace _02Worker
 {
@@ -6,7 +10,50 @@ namespace _02Worker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName= "123.57.233.60" };
+            using (var connection=factory.CreateConnection())
+            {
+                using (var channel=connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "task_queue",
+                        durable: true,//持久化
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    //限定同时只能接受一个消息，(Fair Dispatch)
+                    //这样不会使耗时多的consumer堆积消息
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                    Console.WriteLine(" [x] Waitting for message.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (sender, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+
+                        Console.WriteLine($" [x] received {message}");
+
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+                        Console.WriteLine(" [x] done");
+
+                        //注意一点要加这句，不然会消耗内存
+                        //不加的话：当consumer拒绝消息时，消息会重新发送，但是RabbitMQ不会释放没有确认的消息。
+                        //可通过[sudo] rabbitmqctl[.bat] list_queues name messages_ready messages_unacknowledged调试
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    };
+
+                    channel.BasicConsume(queue: "task_queue",
+                        autoAck: false,
+                        consumer: consumer);
+
+                    Console.WriteLine(" Press any key to exit.");
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
