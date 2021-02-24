4f8a5e8a3cffd144702c0c1b594fe69333a9f344
IronMarmot <35325388+IronMarmot@users.noreply.github.com>
2021-02-24 20:19:43 +0800

    列表反转
    借助3个指针，分别指向prev,current,next，当current!=null时，依次获取next，赋值current.next=pre，并依次向后移动指针即可。

---
+++ Algorithm/LeetCode/反转链表/ReverseList.Test/UnitTest1.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+
+namespace ReverseList.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Class1 class1=new Class1();
+            ListNode head=new ListNode(1);
+            ListNode two=new ListNode(2);
+            ListNode three=new ListNode(3);
+            ListNode four=new ListNode(4);
+            ListNode five=new ListNode(5);
+            head.next=two;
+            two.next=three;
+            three.next=four;
+            four.next=five;
+            five.next=null;
+
+            ListNode node=class1.ReverseList(head);
+            int[] array=new int[5];
+            int i=0;
+            while(node!=null)
+            {
+                array[i]=node.val;
+                i++;
+                node=node.next;
+            }
+
+            Assert.Equal(new int[]{5,4,3,2,1},array);
+        }
+    }
+}
---
+++ Algorithm/LeetCode/反转链表/ReverseList/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ReverseList
+{
+    public class Class1
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode pre=null;
+            ListNode current=head;
+            while(current!=null)
+            {
+                ListNode next=current.next;
+                current.next=pre;
+                pre=current;
+                current=next;
+            }
+
+            return pre;
+        }
+    }
+
+    public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int val=0, ListNode next=null) {
+          this.val = val;
+          this.next = next;
+      }
+  }
+}
