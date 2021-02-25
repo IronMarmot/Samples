a9dd7f9600e50c0c959a4bf379ade44bb41d9859
IronMarmot <35325388+IronMarmot@users.noreply.github.com>
2021-02-25 16:30:23 +0800

    验证回文串
    数组实现和但链表实现

---
+++ Algorithm/LeetCode/回文串判断/Palindrome/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+/*
+验证回文串
+前提：
+1.空串认为是回文串
+2.只对字母和数字进行判断
+
+将字符串转换成char[]，用2个指针从前后依次进行遍历，当当前元素不是字母和数字时移动到下一个，否则进行判断
+
+这里有2个需要注意的：
+1.在while(i)循环中，需要对i进行限制，以避免字符串都是非字母和数字的情况
+2.对字母和数字的判断方法
+*/
+namespace Palindrome
+{
+    public class Class1
+    {
+        public bool IsPalindrome(string str)
+        {
+            if(string.IsNullOrEmpty(str))
+            {
+                return true;
+            }
+
+            char[] chs=str.ToLower().ToCharArray();
+            int i=0,j=chs.Length-1;
+            while(i<j)
+            {
+                while(!IsAlphaOrNumber(chs[i]))
+                {
+                    i++;
+                    if(i==chs.Length) return true;
+                }
+                while(!IsAlphaOrNumber(chs[j]))
+                    j--;
+                if(chs[i]!=chs[j])
+                    return false;
+                i++;
+                j--;
+            }
+            return true;
+        }
+
+        public bool IsAlphaOrNumber(char c)
+        {
+            return (c>='a'&&c<='z')||(c>='0'&&c<='9');
+        }
+    }
+}
---
+++ Algorithm/LeetCode/回文串判断/Palindrome/Class2.cs
@@ -0,0 +1,59 @@
+using System;
+/*
+链表处理回文串判断
+首先找到链表的中间节点，对后半部分链表进行反转，然后依次对链表元素进行比较。
+
+*/
+namespace Palindrome
+{
+    public class Class2
+    {
+        public bool IsPolindrome(ListNode head)
+        {
+            if(head==null||head.next==null)
+                return true;
+
+            //寻找链表的中间节点
+            ListNode slow=head;
+            ListNode quick=head;
+            while(quick!=null&&quick.next!=null)
+            {
+                slow=slow.next;
+                quick=quick.next.next;
+            }
+
+            //反转从slow到链表尾的元素
+            ListNode prev=null;
+            ListNode current=slow;
+            ListNode next=current.next;
+            while(current.next!=null)
+            {
+                current.next=prev;
+                prev=current;
+                current=next;
+                next=next.next;
+            }
+            current.next=prev;
+
+            //此时，反转的链表的head为current
+            while(head!=null&&current!=null)
+            {
+                if(head.val!=current.val)
+                    return false;
+                head=head.next;
+                current=current.next;
+            }
+
+            return true;
+        }
+    }
+
+    public class ListNode{
+        public int val;
+        public ListNode next;
+        public ListNode(int value)
+        {
+            this.val=value;
+        }
+    }
+}
